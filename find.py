ba69db4c18159df656ee873e1617a9b3c4708264
1Michael23 <michael.laginestra0@gmail.com>
2020-12-10 10:56:31 +1100

    intitial commit

---
+++ find.py
@@ -0,0 +1,14 @@
+#finds cockatoos
+
+#modules
+import signal
+import sys
+#import tensorflow #shittage with 3.9
+import cv2
+
+from rich import print as rprint
+from rich.panel import Panel
+Panel.fit("[bold yellow]Hi, I'm a Panel", border_style="red")
+
+#init
+
